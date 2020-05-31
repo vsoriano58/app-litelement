6e17f822130a974808cb9c0a1ad8b0e18ff3742f
Vicente <vsoriano58.com>
2020-05-31 09:44:02 +0200

    Instalar Redux Dev Tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { app } from './reducers/app-reducer.js'
 
 import thunk from 'redux-thunk';
 
+// Sets up a Chrome extension for time travel debugging.
+// See https://github.com/zalmoxisus/redux-devtools-extension for more information.
+const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(
    app,
+      devCompose(
    applyMiddleware(thunk)
+   )
 ) 
