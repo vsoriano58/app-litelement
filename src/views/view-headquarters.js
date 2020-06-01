af5c3e06b088c65a72c6a194a07ec54d2afe6c87
Vicente <vsoriano58.com>
2020-06-01 12:43:08 +0200

    Scroll suave a sedes Madrid y Barcelona

--- src/views/view-headquarters.js
+++ src/views/view-headquarters.js
@@ -5,11 +5,12 @@ import { connect } from 'pwa-helpers';
 import { store } from '../redux/store';
 
 import 'dile-pages/dile-pages'
+import { DileSmoothScrollMixin } from 'dile-smooth-scroll/DileSmoothScrollMixin';
 
 import '../headquarters/headquarters-barcelona-view';
 import '../headquarters/headquarters-madrid-view';
 
-class ViewHeadquarters  extends connect(store)(PageViewElement) {
+class ViewHeadquarters  extends connect(store)(DileSmoothScrollMixin(PageViewElement)) {
 
   static get properties() {
     return {
@@ -34,11 +35,20 @@ class ViewHeadquarters  extends connect(store)(PageViewElement) {
       <div>
         <h2>Sede</h2>
         <p>
-          <a href="/headquarters/madrid">Sede de Madrid</a> |
-          <a href="/headquarters/barcelona" >Sede de Barcelona</a> | 
+          <a href="/headquarters/madrid" @click="${this.headquartersClick}">Sede de Madrid</a> |
+          <a href="/headquarters/barcelona" @click="${this.headquartersClick}">Sede de Barcelona</a> | 
           <a href="/headquarters/bilbao">Sede de Bilbao (no existe)</a> 
         </p>
-     
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem soluta quia perspiciatis beatae magni eius adipisci ullam nulla ipsa, sed laboriosam facere natus? Dignissimos repudiandae repellat autem voluptatibus a.</p>
+        <p>Consequatur molestiae velit laboriosam nesciunt corporis veniam impedit earum quidem quo deserunt aspernatur laudantium optio, atque blanditiis fugit eligendi perspiciatis cum ad, reiciendis dolor labore! Facere ipsa nisi maiores porro.</p>
+        <p>Debitis libero tenetur, quibusdam aperiam adipisci impedit voluptatibus nihil vel dicta porro beatae officiis labore repellendus hic consequuntur error quidem deleniti asperiores ipsa exercitationem ut maxime tempora omnis harum. Culpa?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe exercitationem soluta quia perspiciatis beatae magni eius adipisci ullam nulla ipsa, sed laboriosam facere natus? Dignissimos repudiandae repellat autem voluptatibus a.</p>
+        <p>Consequatur molestiae velit laboriosam nesciunt corporis veniam impedit earum quidem quo deserunt aspernatur laudantium optio, atque blanditiis fugit eligendi perspiciatis cum ad, reiciendis dolor labore! Facere ipsa nisi maiores porro.</p>
+        <p>Debitis libero tenetur, quibusdam aperiam adipisci impedit voluptatibus nihil vel dicta porro beatae officiis labore repellendus hic consequuntur error quidem deleniti asperiores ipsa exercitationem ut maxime tempora omnis harum. Culpa?</p>
+        <p>Tenetur incidunt facilis voluptas iusto voluptates omnis praesentium blanditiis, illum, provident impedit, commodi officia minus veritatis quae ad cumque sed repudiandae cupiditate! Aut, delectus tenetur quidem asperiores error perferendis quisquam?</p>
+        <p>Natus, numquam, odit, quo dicta eum dolores minima et sunt temporibus dignissimos nisi. Aperiam facilis, minima cum quibusdam rerum aspernatur quia perspiciatis itaque recusandae blanditiis odit aliquid maiores optio sequi.</p>
+        
         <dile-pages selected="${this.section}" attrForSelected="name" id="info">
           <headquarters-madrid-view name="madrid" ?active="${this.section == 'madrid'}"></headquarters-madrid-view>
           <headquarters-barcelona-view name="barcelona" ?active="${this.section == 'barcelona'}"></headquarters-barcelona-view>
@@ -46,6 +56,10 @@ class ViewHeadquarters  extends connect(store)(PageViewElement) {
       </div>  
       
     `;
+  }
+
+  headquartersClick(){
+    this.smoothScrollElementIntoView(this.shadowRoot.getElementById('info'));
   }
 
   stateChanged(state) {
